8c3a21a47f7a7e75f5939428700a5d50fc4c7823
Christal Haines <157112981+Christal-1@users.noreply.github.com>
2026-05-18 10:52:26 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,3 +1,8 @@
-API_KEY = "aba0eaf1dcafa9324bec8b8e81ca7ad3"
 CITY = "Johannesburg"
 DB_NAME = "data.db"
+
+try:
+    import streamlit as st
+    API_KEY = st.secrets["API_KEY"]   # for deployed app
+except:
+    API_KEY = "your_local_api_key_here"   # for local testing
